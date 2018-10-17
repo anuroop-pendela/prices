78c2b40198a332866131177ba1a43cedcb8371da
Praneeth Rajarikam <praneethrajarikam@gmail.com>
2018-10-17 11:44:30 +0530

    Added bitmex prices

--- main_application/app_starter.py
+++ main_application/app_starter.py
@@ -2,6 +2,11 @@ import threading
 import requests
 import time
 from main_application.models import Prices
+import traceback
+from .bitmex import BitmexWS
+import logging
+
+
 class priceUpdater:
 
     def __init__(self):
@@ -9,7 +14,7 @@ class priceUpdater:
         Constructor to do noting
         """
         print("Price updater Initialized")
-        self.exchanges_list={}
+        self.exchanges_list = {}
 
         # Store the public url's of the exchanges and traverse through them to store data.
         self.exchanges_list['cexio'] = 'https://cex.io/api/ticker/BTC/USD'
@@ -48,25 +53,66 @@ class priceUpdater:
         self.exchange_meta_data['bitflyer'] = {}
         self.exchange_meta_data['bitflyer']['bid'] = 'best_bid'
         self.exchange_meta_data['bitflyer']['ask'] = 'best_ask'
-        self.exchange_meta_data['bitflyer']['params'] = {'product_code':'BTC_JPY'}
+        self.exchange_meta_data['bitflyer']['params'] = {
+            'product_code': 'BTC_JPY'}
 
         # Metadata for bitflyerFx
         self.exchange_meta_data['bitflyerFx'] = {}
         self.exchange_meta_data['bitflyerFx']['bid'] = 'best_bid'
         self.exchange_meta_data['bitflyerFx']['ask'] = 'best_ask'
-        self.exchange_meta_data['bitflyerFx']['params'] = {'product_code':'FX_BTC_JPY'}
-
-
+        self.exchange_meta_data['bitflyerFx']['params'] = {
+            'product_code': 'FX_BTC_JPY'}
 
         self.__price_updater__()
-        # Initiate a thread to make api reuest and store the data into
-        # self.t1 = threading.Thread(target=self.__price_updater__)
-        # self.t1.setName('startQueuedStrategies')
-        # self.t1.start()
-        pass
 
+        # Bitmex WS variables
+        self.ws = None
+        self.ws_thread = None
+        self.ws_object = None
+        self.ws_restart = False
+        self.t1 = threading.Thread(target=self.handleBitmexWebsocket)
+        self.t1.setName('handleBitmexWebsocket')
+        self.t1.start()
 
-    def  __price_updater__(self):
+    def handleBitmexWebsocket(self, **kwargs):
+        try:
+            self.ws = BitmexWS()
+            self.ws.connect()
+            self.ws_thread = self.ws.wst
+            self.ws_object = self.ws.ws
+            while True:
+                # Something went wrong with the Websocket object
+                if not self.ws:
+                    self.ws = BitmexWS()
+                    self.ws.connect()
+                # Something went wrong with the websocket.WebSocketApp object
+                if not self.ws.ws or not self.ws.ws.sock or not self.ws.ws.sock.connected:
+                    # Restart the Websocket
+                    try:
+                        # Close previous thread
+                        if self.ws_object:
+                            self.ws_object.close()
+                            time.sleep(2)
+                        self.ws.subscribed_to_orderbook = False
+                        self.ws.exit()
+                    except:
+                        logging.error('Exception in handleWebsocket inner loop: {}'.format(
+                            traceback.format_exc()))
+                    time.sleep(2)
+                    self.ws.connect()
+                    self.ws_thread = self.ws.wst
+                    self.ws_object = self.ws.ws
+                # If restart is not required sleep for 1s
+                if not self.ws_restart:
+                    time.sleep(1)
+
+                # If restart is required, skip sleep and recheck
+                self.ws_restart = False
+        except:
+            logging.error("Exception in handleWebsocket main loop: {}".format(
+                traceback.format_exc()))
+
+    def __price_updater__(self):
         """
         Function to initiate storing prices data into database.
         """
@@ -79,19 +125,22 @@ class priceUpdater:
                     params = temp_metadata.get('params')
 
                     temp_prices = Prices()
-                    r = requests.get(url = self.exchanges_list.get(exchange),params = params)
+                    r = requests.get(url=self.exchanges_list.get(
+                        exchange), params=params)
 
                     # extracting data in json format
                     data = r.json()
                     bid_price = data.get(bid_var)
                     ask_price = data.get(ask_var)
 
-                    setattr(temp_prices,'bid',bid_price)
-                    setattr(temp_prices,'ask',ask_price)
-                    setattr(temp_prices,'exchange_name',exchange)
+                    setattr(temp_prices, 'bid', bid_price)
+                    setattr(temp_prices, 'ask', ask_price)
+                    setattr(temp_prices, 'exchange_name', exchange)
                     temp_prices.save()
-                    print(exchange,bid_price,ask_price)
+                    print(exchange, bid_price, ask_price)
             except Exception as ex:
-                print("Faced this exception ",ex)
+                print("Faced this exception ", ex)
+                logging.error('Exception in price_updater: {}'.format(
+                    traceback.format_exc()))
             finally:
                 time.sleep(5)
---
+++ main_application/bitmex.py
@@ -0,0 +1,254 @@
+import websocket
+import time
+import datetime
+import hmac
+import hashlib
+import json
+import threading
+import ssl
+import sys
+import decimal
+import traceback
+import pickle
+from urllib.parse import urlparse, urlunparse
+import logging
+
+
+class BitmexWS(object):
+
+    def __init__(self):
+        self.exchange = 'bitmex'
+        self.__reset()
+        self.message_counter = 0
+        self.base_url = "https://www.bitmex.com/api/v1/"
+        self.data = {}
+        self.keys = {}
+        self.exited = False
+        self._error = None
+        self.ws = None
+        self.wst = None
+        self.ticker_ask = -1
+        self.ticker_bid = -1
+        self.ticker_last = 0
+
+    def connect(self, endpoint="https://www.bitmex.com/api/v1/", symbol="XBTUSD"):
+        '''Connect to the websocket and initialize data stores.'''
+        logging.info("Bitmex_Exchange | Connecting WebSocket")
+        self.symbol = symbol
+
+        # We can subscribe right in the connection querystring, so let's build that.
+        # Subscribe to all pertinent endpoints
+        #subscriptions = [sub + ':' + symbol for sub in ["quote", "trade"]]
+        subscriptions = [sub + ':' + symbol
+                         for sub in ["quote"]]  # "orderBookL2"
+
+        logging.info('Bitmex_Exchange | subscriptions : %s' %
+                     (subscriptions))
+
+        # Get WS URL and connect.
+        urlParts = list(urlparse(endpoint))
+        urlParts[0] = urlParts[0].replace('http', 'ws')
+        urlParts[2] = "/realtime?subscribe=" + ",".join(subscriptions)
+        wsURL = urlunparse(urlParts)
+        logging.info("Bitmex_Exchange | Connecting to %s" % (wsURL))
+        self.__connect(wsURL)
+
+    #
+    # Data methods
+    #
+
+    def error(self, err):
+        self._error = err
+        logging.error("Bitmex_Exchange | Error: %s" % (err))
+        self.exit()
+
+    def exit(self):
+        self.exited = True
+        logging.error(
+            'Bitmex_Exchange | web-socket exit method | the prices here are ask_price : {} | bid price :{} '.format(self.ticker_ask, self.ticker_bid))
+        self.ws.close()
+
+    #
+    # Private methods
+    #
+
+    def __connect(self, wsURL):
+        '''Connect to the websocket in a thread.'''
+        self.ws = websocket.WebSocketApp(
+            wsURL,
+            on_message=self.__on_message,
+            on_close=self.__on_close,
+            on_open=self.__on_open,
+            on_error=self.__on_error,
+        )
+
+        self.wst = threading.Thread(target=lambda: self.ws.run_forever())
+        self.wst.daemon = True
+        self.wst.setName('bitmex_ws.run_forever')
+        self.wst.start()
+        # Wait for connect before continuing
+        conn_timeout = 5
+        try:
+            while (not self.ws.sock or not self.ws.sock.connected) and conn_timeout and not self._error:
+                time.sleep(1)
+                conn_timeout -= 1
+            if not conn_timeout or self._error:
+                logging.error(
+                    "Bitmex_Exchange | Couldn't connect to WS! Exiting System. | Error : %s" % (self._error))
+
+                self.exit()
+
+        except:
+
+            logging.error(
+                "Bitmex_Exchange | Error in connection: {}" .format(traceback.format_exc()))
+
+    def __on_message(self, ws, message):
+        '''Handler for parsing WS messages.'''
+        message = json.loads(message)
+        # log message
+        logging.info(
+            "Bitmex_Exchange | json.dumps(message) = %s" % (json.dumps(message)))
+        self.message_counter += 1
+        table = message['table'] if 'table' in message else None
+        action = message['action'] if 'action' in message else None
+
+        try:
+            if 'subscribe' in message:
+                if message['success']:
+                    logging.info(
+                        "Bitmex_Exchange | Subscribed to %s." % (message['subscribe']))
+                else:
+
+                    self.error(
+                        "Bitmex_Exchange | Unable to subscribe to %s. Error: \"%s\" Please check and restart."
+                        % (message['request']['args'][0], message['error']))
+
+            elif 'status' in message:
+                if message['status'] == 400:
+
+                    self.error(message['error'])
+                if message['status'] == 401:
+
+                    self.error(
+                        "Bitmex_Exchange | API Key incorrect, please check and restart.")
+            elif action:
+
+                if table not in self.data:
+                    self.data[table] = []
+
+                if table not in self.keys:
+                    self.keys[table] = []
+
+                # There are four possible actions from the WS:
+                # 'partial' - full table image
+                # 'insert'  - new row
+                # 'update'  - update row
+                # 'delete'  - delete row
+                if action == 'partial':
+                    #logging.info("Bitmex_Exchange | %s: partial" %(table))
+                    self.data[table] += message['data']
+                    # Keys are communicated on partials to let you know how to uniquely identify
+                    # an item. We use it for updates.
+                    self.keys[table] = message['keys']
+                elif action == 'insert':
+                    # logging.info('Bitmex_Exchange | %s: inserting %s'
+                    #                  % ( table,
+                    #                     message['data']))
+                    self.data[table] += message['data']
+
+                    # Limit the max length of the table to avoid excessive memory usage.
+                    # Don't trim orders because we'll lose valuable state if we do.
+
+                elif action == 'update':
+                    #logging.info('Bitmex_Exchange | %s: updating %s'% ( table,message['data']))
+                    # Locate the item in the collection and update it.
+                    for updateData in message['data']:
+                        item = findItemByKeys(self.keys[table],
+                                              self.data[table], updateData)
+                        if not item:
+                            continue  # No item found to update. Could happen before push
+
+                        # Log executions
+
+                        # Update this item.
+                        item.update(updateData)
+
+                elif action == 'delete':
+                    #logging.info('Bitmex_Exchange | %s: deleting %s'% (table,message['data']))
+                    # Locate the item in the collection and remove it.
+                    for deleteData in message['data']:
+                        item = findItemByKeys(self.keys[table],
+                                              self.data[table], deleteData)
+                        try:
+                            self.data[table].remove(item)
+
+                        except:
+
+                            logging.error('Bitmex_Exchange | exception on %s 2' % (
+                                self.data[table].remove(item)))
+                else:
+                    raise Exception(
+                        "Bitmex_Exchange | Unknown action: %s" % (action))
+
+                # do at this indent level
+                #
+
+                data_dict = self.data
+                if table == 'quote':
+                    if 'askPrice' in data_dict['quote'][-1]:
+                        if abs(data_dict['quote'][-1]['askPrice'] - self.ticker_last) >= 0.1:
+                            print('data_dict: ', data_dict)
+                            self.ticker_last = data_dict['quote'][-1]['askPrice']
+                            self.ticker_bid = data_dict['quote'][-1]['bidPrice']
+                            self.ticker_ask = data_dict['quote'][-1]['askPrice']
+
+                            logging.info("Bitmex_Exchange | websocket updated ask_price: {} and bid_price: {}" .format(
+                                self.ticker_ask, self.ticker_bid))
+                    else:
+                        logging.warning('Bitmex_Exchange | No ask price')
+        except:
+
+            logging.error('Bitmex_Exchange | Exception : %s' %
+                          (traceback.format_exc()))
+
+    def __on_open(self, ws):
+        logging.info("Bitmex_Exchange | Websocket Opened.")
+
+    def __on_close(self, ws):
+        logging.error('Bitmex_Exchange | Websocket Closed')
+        self.exit()
+
+    def __on_error(self, ws, error):
+        if not self.exited:
+            self.error(error)
+
+    def __reset(self):
+        self.data = {}
+        self.keys = {}
+        self.exited = False
+        self._error = None
+
+    def get_ticker(self):
+        try:
+            response = self.data['quote'][-1]
+            self.ticker_ask = response.get('askPrice')
+            self.ticker_bid = response.get('bidPrice')
+        except:
+            logging.error('Exception in get_ticker: {}'.format(
+                traceback.format_exc()))
+
+
+def findItemByKeys(keys, table, matchData):
+    for item in table:
+        matched = True
+        for key in keys:
+            if item[key] != matchData[key]:
+                matched = False
+        if matched:
+            return item
+
+
+if __name__ == "__main__":
+
+    b = BitmexWS()
